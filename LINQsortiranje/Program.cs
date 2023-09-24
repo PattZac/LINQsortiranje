8a81e09f10b9241eefb09b354f077eb7876206f2
Pat <113007904+PattZac@users.noreply.github.com>
2023-09-24 19:06:42 +0200

    Add files via upload

---
+++ LINQsortiranje/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQsortiranje
+{
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] studentArray = {
+            new Student() { StudentID = 1, StudentName = "John", Age = 18 },
+            new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 },
+            new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 },
+            new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 },
+            new Student() { StudentID = 5, StudentName = "Ron" , Age = 31 },
+            new Student() { StudentID = 6, StudentName = "Chris",  Age = 17 },
+            new Student() { StudentID = 7, StudentName = "Rob",Age = 19  },
+            };
+
+            var qSort = from s in studentArray
+                        where s.Age > 12 && s.Age < 20
+                        orderby s.Age descending
+                        select s;
+
+            foreach (var s in qSort)
+            {
+                Console.WriteLine(s.StudentName);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
